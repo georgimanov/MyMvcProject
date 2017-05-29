2d3c1c416b2cf7d449e20df5bcac8f4b472255bf
Georgi Manov <georgimanov@gmail.com>
2017-05-29 21:47:20 +0300

    added web project

---
+++ MyMvcProject/MyMvcProject.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyMvcProject.Web.Startup))]
+namespace MyMvcProject.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
